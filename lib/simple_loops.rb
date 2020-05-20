f9c44871f7d626b84edd90675a17181162116bb6
Cesar Lara <cesar.lara@yale.edu>
2020-05-20 21:08:09 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -2,10 +2,10 @@
 
 def loop_message_five_times(message)
   count = 0
-  while count < 5 do 
-    puts message 
+  while count < 5 do
+    puts message
     count += 1
-  end 
+  end
 end
 
 def loop_message_n_times
